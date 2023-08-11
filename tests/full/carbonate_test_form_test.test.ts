c706cca3083a8fdd81391c445cb0fc242900d718
Lewis Wright <LewisW@users.noreply.github.com>
2023-08-11 21:29:11 +0100

    Change full test to match documentation

--- tests/full/carbonate_test_form_test.test.ts
+++ tests/full/carbonate_test_form_test.test.ts
@@ -2,26 +2,33 @@ import { describe, expect, test, beforeAll, afterAll, beforeEach, afterEach } fr
 import SDK from "../../src/SDK";
 import Api from "../../src/api/api"
 import Puppeteer from "../../src/browser/puppeteer"
-import puppeteer, {Browser as Client, ElementHandle, Page} from 'puppeteer';
 
 let browser = new Puppeteer(page);
-let sdk = new SDK(browser);
+let sdk = new SDK(browser, null, null, null, null, new Api(null, null, 'https://api.staging.carbonate.dev/'));
 
 setSDK(sdk);
 
 describe("CarbonateTestFormTest", () => {
-    test("Birthday event type", async () => {
+    test("Select birthday from the event type dropdown", async () => {
         await sdk.load(
-            'https://carbonate.dev/demo-form'
+            'https://carbonate.dev/demo-form.html'
         );
 
-        await sdk.action('chose Birthday as the event type')
+        await sdk.action('select Birthday from the event type dropdown')
 
         expect(
-            await sdk.assertion('the event type should be Birthday')
+            await sdk.assertion('the event type dropdown should be set to Birthday')
         ).toBe(true);
+    });
 
-        let dropdown = await sdk.lookup('the event type dropdown');
+    test("Select birthday from the event type dropdown advanced", async () => {
+        await sdk.load(
+            'https://carbonate.dev/demo-form.html'
+        );
+
+        const dropdown = await sdk.lookup('The dropdown')
+
+        await dropdown.select('Birthday')
 
         expect(
             await (await dropdown.getProperty('value')).jsonValue()
