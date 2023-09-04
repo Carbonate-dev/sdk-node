87a0f016d177b56191ada7c34f3c67a38062789b
Lewis Wright <LewisW@users.noreply.github.com>
2023-09-04 23:06:11 +0100

    Remove directory imports

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import SDK from './SDK';
-import * as browser from './browser';
+import * as browser from './browser/index';
 import * as exceptions from './exceptions/exceptions';
-import * as jest from './jest';
-import * as logger from './logger';
+import * as jest from './jest/index';
+import * as logger from './logger/index';
 
 export default SDK;
 export { browser, exceptions, jest, logger };
