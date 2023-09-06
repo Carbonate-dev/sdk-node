c4791680908e1000b6a871ab0a07ccdbfab9fcc1
Lewis Wright <LewisW@users.noreply.github.com>
2023-09-06 01:11:59 +0100

    Ditch default export

--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,4 @@ export { default as Puppeteer } from './browser/puppeteer';
 export * from './exceptions/exceptions';
 export { default as Logger } from './logger/logger';
 export { default as TestLogger } from './logger/test_logger';
-export { default } from './SDK';
+export { default as SDK } from './SDK';
