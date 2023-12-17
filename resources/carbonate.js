8620b64329d71a773502b11990a34a71140b4e9d
Lewis Wright <LewisW@users.noreply.github.com>
2023-12-17 19:45:09 +0000

    Use ShadyDOM to build the HTML and strip elements at source (including hidden)

--- resources/carbonate.js
+++ resources/carbonate.js
@@ -179,17 +179,320 @@
     // Let Carbonate query for hidden elements
 
     window.carbonate_getAllHiddenElements = function (el) {
-        const all = el.getElementsByTagName("*");
-        let hidden = [];
-
-        for (let i = 0, max = all.length; i < max; i++) {
-            if (all[i].offsetWidth === 0 && all[i].offsetHeight === 0) {
-                hidden.push(getXPathForElement(all[i]));
-            }
+        if (el.offsetWidth === 0 && el.offsetHeight === 0) {
+            return [el];
         }
 
-        return hidden;
+        let children = Array.from(el.children);
+        let hiddenChildren = children.map(window.carbonate_getAllHiddenElements).flat();
+
+        return hiddenChildren;
     }
+
+    /**
+     @license
+     Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
+     This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+     The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+     The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+     Code distributed by Google as part of the polymer project is also
+     subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+     */
+    // https://github.com/webcomponents/polyfills/blob/90cb97f847ce918289dac0978c50dcda0a0afd72/packages/shadydom/src/innerHTML.js (with modifications)
+
+    // http://www.whatwg.org/specs/web-apps/current-work/multipage/the-end.html#escapingString
+    let escapeAttrRegExp = /[&\u00A0"]/g;
+    let escapeDataRegExp = /[&\u00A0<>]/g;
+
+    function escapeReplace(c) {
+        switch (c) {
+            case '&':
+                return '&amp;';
+            case '<':
+                return '&lt;';
+            case '>':
+                return '&gt;';
+            case '"':
+                return '&quot;';
+            case '\u00A0':
+                return '&nbsp;';
+        }
+    }
+
+    function escapeAttr(s) {
+        return s.replace(escapeAttrRegExp, escapeReplace);
+    }
+
+    function escapeData(s) {
+        return s.replace(escapeDataRegExp, escapeReplace);
+    }
+
+    function makeSet(arr) {
+        let set = {};
+        for (let i = 0; i < arr.length; i++) {
+            set[arr[i]] = true;
+        }
+        return set;
+    }
+
+    // http://www.whatwg.org/specs/web-apps/current-work/#void-elements
+    let voidElements = makeSet([
+        'area',
+        'base',
+        'br',
+        'col',
+        'command',
+        'embed',
+        'hr',
+        'img',
+        'input',
+        'keygen',
+        'link',
+        'meta',
+        'param',
+        'source',
+        'track',
+        'wbr',
+    ]);
+
+    let plaintextParents = makeSet([
+        'xmp',
+        'iframe',
+        'noembed',
+        'noframes',
+        'plaintext',
+        'noscript',
+    ]);
+
+    const tagBlacklist = {
+        '!doctype': true,
+        '!--': true,
+        'base': true,
+        'style': true,
+        'rel': true,
+        'link': true,
+        'script': true,
+        'noscript': true,
+        'title': true,
+        'head': true,
+        'form': [
+            'accept-charset',
+            'enctype',
+            'novalidate',
+        ],
+        'iframe': [
+            'allow',
+        ],
+        'a': [
+            'target',
+        ],
+        'meter': [
+            'high',
+            'low',
+            'max',
+            'min',
+            'optimum',
+        ],
+        'audio': [
+            'autoplay',
+            'controls',
+            'loop',
+            'muted',
+            'preload',
+            'buffered',
+        ],
+        'video': [
+            'autoplay',
+            'controls',
+            'loop',
+            'muted',
+            'preload',
+            'buffered',
+            'playsinline',
+        ],
+        'applet': [
+            'code',
+        ],
+        'table': [
+            'colspan',
+            'rowspan',
+        ],
+        'meta': true,
+        'area': true,
+        'svg': [
+            'xmlns',
+            'xmlns:xlink',
+            'version',
+            'baseprofile',
+            'viewbox',
+            'preserveaspectratio',
+            'x',
+            'y',
+            'fill',
+            'stroke',
+            'stroke-width',
+            'stroke-linecap',
+            'stroke-linejoin',
+            'stroke-miterlimit',
+            'stroke-dasharray',
+            'stroke-dashoffset',
+            'stroke-opacity',
+            'fill-rule',
+            'clip-rule',
+            'opacity',
+            'enable-background',
+            'transform',
+            'color',
+            'color-interpolation',
+            'color-interpolation-filters',
+            'color-profile',
+            'color-rendering',
+            'cursor',
+            'direction',
+            'display',
+            'dominant-baseline',
+            'image-rendering',
+            'letter-spacing',
+            'pointer-events',
+            'shape-rendering',
+            'text-anchor',
+            'text-decoration',
+            'text-rendering',
+            'unicode-bidi',
+            'visibility',
+            'word-spacing',
+            'writing-mode',
+            'externalresourcesrequired',
+        ],
+        'path': true,
+        'object': true,
+        'img': [
+            'decoding',
+            'ismap',
+        ],
+        'textarea': [
+            'rows',
+            'cols',
+            'wrap',
+        ],
+        'track': true,
+        '*': [
+            'align',
+            'autocapitalize',
+            'autocomplete',
+            'autocorrect',
+            'autofocus',
+            'background',
+            'bgcolor',
+            'border',
+            'dir',
+            'cite',
+            'crossorigin',
+            'csp',
+            'height',
+            'spellcheck',
+            'style',
+            'shape',
+            'lang',
+            'language',
+            'loading',
+            'maxlength',
+            'minlength',
+            'referrerpolicy',
+            'rel',
+            'slot',
+            'translate',
+            'valign',
+            'width',
+        ],
+    }
+
+    const childBlacklist = makeSet([
+        'svg',
+    ]);
+
+    /**
+     * @param {Node} node
+     * @param {Node} parentNode
+     * @param {Function=} callback
+     */
+    window.carbonate_getOuterHTML = function(node, parentNode, callback) {
+        if (node.offsetWidth === 0 && node.offsetHeight === 0) {
+            return '';
+        }
+
+        switch (node.nodeType) {
+            case Node.ELEMENT_NODE: {
+                let tagName = node.localName;
+
+                if (tagBlacklist[tagName] === true) {
+                    console.debug('Skipping tag', tagName);
+                    return '';
+                }
+
+                let s = '<' + tagName;
+                let attrs = node.attributes;
+                for (let i = 0, attr; (attr = attrs[i]); i++) {
+                    let isData = attr.value.indexOf('data:') === 0;
+                    let isBlacklist = tagBlacklist[tagName] && tagBlacklist[tagName].indexOf(attr.name) !== -1;
+                    let isGlobalBlacklist = tagBlacklist['*'] && tagBlacklist['*'].indexOf(attr.name) !== -1;
+                    if (isGlobalBlacklist || isBlacklist || isData) {
+                        console.debug('Skipping attribute', attr.name, 'on', tagName);
+                        continue;
+                    }
+                    s += ' ' + attr.name + '="' + escapeAttr(attr.value) + '"';
+                }
+                s += '>';
+                if (voidElements[tagName]) {
+                    return s;
+                }
+                if (childBlacklist[tagName]) {
+                    console.debug('Skipping children of', tagName);
+                    return s + '</' + tagName + '>';
+                }
+                return s + window.carbonate_getInnerHTML(node, callback) + '</' + tagName + '>';
+            }
+            case Node.TEXT_NODE: {
+                let data = /** @type {Text} */ (node).data;
+                if (parentNode && plaintextParents[parentNode.localName]) {
+                    return data;
+                }
+                return escapeData(data);
+            }
+            case Node.COMMENT_NODE: {
+                return '<!--' + /** @type {Comment} */ (node).data + '-->';
+            }
+            default: {
+                window.console.error(node);
+                throw new Error('not implemented');
+            }
+        }
+    }
+
+    /**
+     * @param {Node} node
+     * @param {Function=} callback
+     */
+    window.carbonate_getInnerHTML = function(node, callback) {
+        let shadowRoot = node.shadowRoot;
+
+        if (shadowRoot) {
+            node = shadowRoot
+        }
+
+        if (node.localName === 'template') {
+            node = /** @type {HTMLTemplateElement} */ (node).content;
+        }
+        let s = '';
+        let c$ = callback ? callback(node) : node.childNodes;
+        for (let i = 0, l = c$.length, child; i < l && (child = c$[i]); i++) {
+            s += window.carbonate_getOuterHTML(child, node, callback);
+        }
+        return shadowRoot ? `<template shadowroot="${shadowRoot.mode}">${s}</template>` : s;
+    }
+    window.carbonate_getOuterHTML(window.document.body);
+
+    // END OF SHADYDOM CODE
 
     // Based off: https://stackoverflow.com/a/46781845/6662
     function getXPathForElement(element) {
